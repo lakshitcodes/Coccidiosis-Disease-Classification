0416290f3bc139cb410b6a1a2f60a83d236c9aa4
Lakshit Jain <jainlakshit849@gmail.com>
2025-08-19 19:45:21 +0530

    Requirements and setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__="0.0.0"
+
+REPO_NAME="Coccidiosis-Disease-Classification"
+AUTHOR_USER_NAME="lakshitcodes"
+SRC_REPO="Coccidiosis-Disease-Classification"
+AUTHOR_EMAIL="jainlakshit849@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lakshitcodes/Coccidiosis-Disease-Classification",
+    project_urls={
+        "Bug_Tracker": "https://github.com/lakshitcodes/Coccidiosis-Disease-Classification/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
