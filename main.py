f5c5e66084ad943c431cd2d28bf04f4c87680678
Lakshit Jain <jainlakshit849@gmail.com>
2025-08-20 21:26:28 +0530

    Prepare Base Model Completed

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from coccidiosisDiseaseClassification import logger
 from coccidiosisDiseaseClassification.pipeline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
+from coccidiosisDiseaseClassification.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
 
 STAGE_NAME = "Data Ingestion Pipeline"
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
